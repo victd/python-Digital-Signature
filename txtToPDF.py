fbbb9bc4982944c9e502545dee8f7780dbcd6d82
victd <55512729+victd@users.noreply.github.com>
2023-06-09 09:09:10 -0400

    Update txtToPDF.py

--- txtToPDF.py
+++ txtToPDF.py
@@ -1,6 +1,6 @@
 # digital Signature
 # set API interface for user input or piped program
-# custom workflow 
+# custom workflow, streamlined
 # implement handling of security certificates
 # Personal Information Protection and Electronic Documents Act PIPEDA
 # retention and purge of > 2 year old customer data
